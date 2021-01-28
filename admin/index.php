c9a81cdb582611401a8d671f5dbd74102bb3dad4
Santiago Quintero <quinterosantiago15@gmail.com>
2021-01-28 11:52:55 -0500

    reading data from DB and show

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,17 @@
 <?php 
+    // ? importar la conexión a la DB
+    require '../includes/config/database.php';
+    $db = connectDB();
 
+    // ? escribir el query
+    $query = "SELECT * FROM properties";
+
+    // ? Consultar la BD
+    $responseQuery = mysqli_query($db, $query);
+
+    // ? Muestra mensaje condicional
     $resultado = $_GET['resultado'] ?? null;
+    // ? incluye un template
     require '../includes/functions.php';
     addTemplate('header');
 ?>
@@ -12,6 +23,37 @@
         <?php endif ?>
 
         <a href="/admin/properties/create.php" class="button green-button">Nueva Propiedad</a>
+        
+        <table class="properties">
+            <thead>
+                <th>Id</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </thead>
+
+            <!-- Mostrar los resultados -->
+            <tbody>
+                <?php while($property = mysqli_fetch_assoc($responseQuery)) : ?>
+                    <tr>
+                        <td><?php echo $property['id']; ?></td>
+                        <td><?php echo $property['title']; ?></td>
+                        <td><img class="table-image" src="/images/<?php echo $property['image']; ?>" alt="Imagen casa en la playa"></td>
+                        <td>$ <?php echo $property['price']; ?></td>
+                        <td>
+                            <a href="#" class="red-button-block">Eliminar</a>
+                            <a href="#" class="yellow-button-block">Actualizar</a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
     </main>
 
-<?php addTemplate('footer'); ?>
+<?php 
+    // ? Cerrar la conexión a la DB
+    mysqli_close($db);
+
+    addTemplate('footer'); 
+?>
